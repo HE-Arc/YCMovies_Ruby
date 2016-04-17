c1ec4dd47e110566a7f948fc88a22605c922a6d6
Bastien Burri <bastien.burri@he-arc.ch>
2016-04-17 21:34:58 +0200

    encore un commit

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
 
   get '/about', :to => 'pages#about'
   get '/contact', :to => 'pages#contact'
+  get '/admin', :to => 'pages#admin'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
